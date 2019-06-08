564096f17841ce344f7ae1012a8fd0ac8dcba222
adj-dev <andrew.johnson2073@gmail.com>
2019-06-07 23:58:51 -0500

    Added definition of models

---
+++ models/lesson.js
@@ -0,0 +1,24 @@
+module.exports = function (sequelize, DataTypes) {
+  // Instantiate the Lesson model
+  const Lesson = sequelize.define("Lesson", {
+    topic: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isEmpty: { msg: "Lesson `title` value cannot be an empty string" }, // don't allow empty strings
+        isAlphanumeric: { msg: "Lesson `title` value must contain only letters and/or numbers" } // will only allow alphanumeric characters
+      }
+    },
+    body: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    }
+  });
+
+  Lesson.associate = function (models) {
+    // Associate with Student model
+    Lesson.belongsTo(models.Student, { foreignKey: { allowNull: false } });
+  };
+
+  return Lesson;
+};
