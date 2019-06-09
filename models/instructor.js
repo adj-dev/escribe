b2ce5159ed9d64b0260f92b760ea9a92210920eb
adj-dev <andrew.johnson2073@gmail.com>
2019-06-09 17:09:01 -0500

    Fixed an issue where allowNull was not being recognized as a valid option for validation

--- models/instructor.js
+++ models/instructor.js
@@ -3,31 +3,31 @@ module.exports = function (sequelize, DataTypes) {
   const Instructor = sequelize.define("Instructor", {
     firstName: {
       type: DataTypes.STRING,
+      allowNull: false, // won't allow null
       validate: {
-        allowNull: false, // won't allow null
         notEmpty: { msg: "Instructor `firstName` value cannot be an empty string" }, // don't allow empty strings
         isAlpha: { msg: "Instructor `firstName` value must contain only letters" } // will only allow letters
       }
     },
     lastName: {
       type: DataTypes.STRING,
+      allowNull: false,
       validate: {
-        allowNull: false,
         notEmpty: { msg: "Instructor `lastName` value cannot be an empty string" },
         isAlpha: { msg: "Instructor `lastName` value must contain only letters" }
       }
     },
     email: {
       type: DataTypes.STRING,
+      allowNull: false,
       validate: {
-        allowNull: false,
         isEmail: { msg: "Instructor `email` value needs to be in standard email format e.g. foo@bar.com" } // checks for email format
       }
     },
     phone: {
       type: DataTypes.STRING,
+      allowNull: false,
       validate: {
-        allowNull: false,
         notEmpty: { msg: "Instructor `phone` value cannot be an empty string" },
         isNumeric: { msg: "Instructor `phone` value must contain only numbers" } // only allows numbers
       }
