178f3776fc0c3e4f7f44b355c3681791957e6dac
adj-dev <andrew.johnson2073@gmail.com>
2019-06-10 12:03:07 -0500

    Major model overhaul

--- models/index.js
+++ models/index.js
@@ -1,42 +1,21 @@
-"use strict";
+const sequelize = require("../config");
+const Instructor = require("./instructor.js");
+const Student = require("./student.js");
+const Lesson = require("./lesson.js");
 
-var fs = require("fs");
-var path = require("path");
-var Sequelize = require("sequelize");
-var basename = path.basename(module.filename);
-var env = process.env.NODE_ENV || "development";
-var config = require(__dirname + "/../config/config.json")[env];
-var db = {};
 
-if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
-} else {
-  var sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
-  );
-}
 
-fs.readdirSync(__dirname)
-  .filter(function(file) {
-    return (
-      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
-    );
-  })
-  .forEach(function(file) {
-    var model = sequelize.import(path.join(__dirname, file));
-    db[model.name] = model;
-  });
+// Make associations
+Instructor.associate({ Student });
+Student.associate({ Instructor, Lesson });
+Lesson.associate({ Student });
 
-Object.keys(db).forEach(function(modelName) {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
+// Sync with the DB
+let syncConfig = { force: false };
+// uncomment the line below to reset all tables for development purposes. 
+// syncConfig.force = true;
+sequelize.sync(syncConfig);
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
 
-module.exports = db;
+
+module.exports = { Instructor, Student, Lesson };
--- models/lesson.js
+++ models/lesson.js
@@ -1,24 +1,31 @@
-module.exports = function (sequelize, DataTypes) {
-  // Instantiate the Lesson model
-  const Lesson = sequelize.define("Lesson", {
-    topic: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        isEmpty: { msg: "Lesson `title` value cannot be an empty string" }, // don't allow empty strings
-        isAlphanumeric: { msg: "Lesson `title` value must contain only letters and/or numbers" } // will only allow alphanumeric characters
-      }
-    },
-    body: {
-      type: DataTypes.TEXT,
-      allowNull: false
+// Pull in sequelize and db connection
+const Sequelize = require("sequelize");
+const sequelize = require("../config");
+
+
+
+// Instantiate the Lesson model
+const Lesson = sequelize.define("Lesson", {
+  topic: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isEmpty: { msg: "Lesson `title` value cannot be an empty string" }, // don't allow empty strings
+      isAlphanumeric: { msg: "Lesson `title` value must contain only letters and/or numbers" } // will only allow alphanumeric characters
     }
-  });
+  },
+  body: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  }
+});
 
-  Lesson.associate = function (models) {
-    // Associate with Student model
-    Lesson.belongsTo(models.Student, { foreignKey: { allowNull: false } });
-  };
-
-  return Lesson;
+Lesson.associate = function (models) {
+  Lesson.belongsTo(models.Student, { foreignKey: { allowNull: false } });
 };
+
+
+
+
+
+module.exports = Lesson;
