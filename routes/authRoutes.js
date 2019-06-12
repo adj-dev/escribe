6a8c35fd1f6da967128cb4ca1f9269efd4f76652
johniblake <johniblake93@gmail.com>
2019-06-12 17:56:22 -0500

    protected all api routes from snooping snoopers

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,14 +1,15 @@
 var router = require("express").Router();
 var db = require("../models");
+const sendmail = require("sendmail")();
 
-module.exports = function (passport) {
-  router.get("/whodis", function (req, res) {
+module.exports = function(passport) {
+  router.get("/whodis", function(req, res) {
     res.render("auth");
   });
   router.post(
     "/whodis",
     passport.authenticate("local"), //this is the magic
-    function (req, res) {
+    function(req, res) {
       // If this function gets called, authentication was successful.
       // `req.user` contains the authenticated user.
       //res.sendFile()
@@ -17,29 +18,42 @@ module.exports = function (passport) {
     }
   );
 
-  //   router.post("/whodis", function(req, res) {
-  //     passport.authenticate("local", function(err, user, info) {
-  //       if (err) {
-  //         return res.json(err);
-  //       }
-  //       if (!user) {
-  //         console.log(info.message);
-  //         return res.json(info);
-  //       } else {
-  //         return res.json({ message: "Success" });
-  //       }
-  //     })(req, res);
-  //   });
-
-  // Create a new Instructor
-  router.post("/signup", function (req, res) {
+  router.post("/signup", function(req, res) {
     // grab the data out of req.body
     let { firstName, lastName, email, phone, password } = req.body;
     // create a new user
-    db.User.create({ email, password, isInstructor: true }).then(function (user) {
+    let newUser;
+    db.User.create({ email, password, isInstructor: true }).then(function(
+      user
+    ) {
       let id = user.id;
-      db.Instructor.create({ firstName, lastName, email, phone, password, UserId: id }).then(function (instructor) {
-        res.json(instructor);
+      newUser = user;
+      sendmail(
+        {
+          from: "admin@enscribe.io",
+          to: user.email,
+          subject: "Welcome to Enscribe",
+          html:
+            "Your login info:<br>email: " +
+            user.email +
+            "<br>password: " +
+            user.password +
+            "<br><br>Click <a href='https://pure-wave-91989.herokuapp.com/' target='_blank'>here</a> to log in."
+        },
+        function(err, reply) {
+          console.dir(reply);
+          console.log("There");
+        }
+      );
+      db.Instructor.create({
+        firstName,
+        lastName,
+        email,
+        phone,
+        password,
+        UserId: id
+      }).then(function(instructor) {
+        res.json(newUser);
       });
     });
   });
@@ -47,7 +61,7 @@ module.exports = function (passport) {
   router.post(
     "/logout",
 
-    function (req, res) {
+    function(req, res) {
       req.logout();
 
       res.json({
