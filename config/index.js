c6515e2d3089ccb389be7a206108fd5d911b2b86
adj-dev <andrew.johnson2073@gmail.com>
2019-06-12 11:45:03 -0500

    renamed environment variable

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ const config = require(__dirname + "/../config/config.js")[env];
 let sequelize;
 
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.useEnvVariable]);
+  sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
